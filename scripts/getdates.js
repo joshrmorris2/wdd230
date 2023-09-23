f3f81219bad51ae2745723a5f7e4ff7484d907cd
joshrmorris2 <moofase1@gmail.com>
2023-09-23 15:32:33 -0700

    fixed javascript dates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,2 @@
-const lastModified = Date.parse(document.lastModified);
+document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
 document.getElementById("year").innerHTML = new Date().getFullYear();
