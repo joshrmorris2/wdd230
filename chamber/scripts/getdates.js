9ca0a6b41f43887d0ad87d67b83b1da48d879dd6
joshrmorris2 <moofase1@gmail.com>
2023-10-14 23:44:15 -0700

    improved chamber site and added dark mode

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,10 +1,25 @@
 document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
+/*Navigation menu*/
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
+});
+
+/*Dark mode*/
+const switchBox = document.querySelector('.switch');
+const body = document.querySelector('body');
+
+switchBox.addEventListener('click', () => {
+    if (switchBox.checked = true) {
+        body.style.background = '#000';
+        body.style.color = '#F3EAF4';
+    } if (switchBox.checked = false) {
+        body.style.background = '#F3EAF4';
+        body.style.color = '#000';
+    }
 });
