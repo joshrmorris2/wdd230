120434752a34392833ad6a2419ae82d253225c41
joshrmorris2 <moofase1@gmail.com>
2023-11-09 16:21:00 -0800

    Finished first JSON assignment Week 7 Chaber update all but the 6 images complete

--- chamber/scripts/lastvisit.js
+++ chamber/scripts/lastvisit.js
@@ -10,6 +10,22 @@ const todayElement = document.querySelector("#lastvisit");
 const today = Date.now();
 const lastVisit = localStorage.getItem('lastTimeVisited');
 
-let message = ;
+function visitMessage() {
+    const timesinceVisited = today - lastVisit;
+    if (!lastVisit) {
+        // First page visit
+        todayElement.textContent = "Welcome! Let us know if you have any questions.";
+    } else {
+        if (timesinceVisited < msToDays) {
+            // Less than a day
+            todayElement.textContent = "Back so soon! Awesome!";
+        } else {
+            // More than a day
+            const pluralize = Math.round(timesinceVisited / msToDays) === 1 ? "day" : "days";
+            todayElement.textContent = "You last visited " + (Math.round(timesinceVisited / msToDays)) + " ${pluralize} ago.";
+        }
+    }
+    localStorage.setItem('lastTimeVisited', today);
+};
 
-todayElement.textContent = message;
+visitMessage();
