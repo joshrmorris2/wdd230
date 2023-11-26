75945014af34a7ebad861c467a0f5656dee04473
joshrmorris2 <moofase1@gmail.com>
2023-11-26 14:43:43 -0800

    removed windshill.js

--- chamber/scripts/windchill.js
+++
@@ -1,7 +0,0 @@
-let airTemperature = 50;
-let windSpeed = 3 ^ .06;
-
-let chill = 35.74 + (0.6215 * airTemperature) - (35.75 * windSpeed) + (0.4275 * airTemperature * windSpeed);
-
-
-document.getElementById("windchill").innerHTML = chill.toFixed(2);
