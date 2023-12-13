4118b4512a3adfbff67cef4b1cf5259ef935d3f5
joshrmorris2 <moofase1@gmail.com>
2023-12-12 21:33:21 -0800

    priceinfo.js prices.json

--- scoots/scripts/priceinfo.js
+++ scoots/scripts/priceinfo.js
@@ -1,15 +1,39 @@
-const pricetable = document.querySelector('pricetable');
+const priceUrl = 'data/prices.json';
+const pricetable = document.querySelector('#pricetable');
 
 async function getPriceData() {
-    const response = await fetch(pricetable);
+    const response = await fetch(priceUrl);
     const data = await response.json();
 
     displayPriceTable(data.vehicles);
 }
 
 displayPriceTable = (vehicles) => {
+    vehicles.forEach((vehicle) => {
+        let row = document.createElement('tr');
+        let type = document.createElement('td');
+        let seats = document.createElement('td');
+        let walkhalf = document.createElement('td');
+        let reshalf = document.createElement('td');
+        let walkfull = document.createElement('td');
+        let resfull = document.createElement('td');
 
-    vehicles.forEach((member) => {
-        let
+        type.textContent = `${vehicle.make} ${vehicle.model} ${vehicle.type}`;
+        seats.textContent = `${vehicle.seats}`;
+        walkhalf.textContent = vehicle.halfday[0].walk;
+        reshalf.textContent = vehicle.halfday[0].reserve;
+        walkfull.textContent = vehicle.fullday[0].walk;
+        resfull.textContent = vehicle.fullday[0].reserve;
+
+        row.append(type,
+            seats,
+            walkhalf,
+            reshalf,
+            walkfull,
+            resfull);
+
+        pricetable.appendChild(row);
     });
 }
+
+getPriceData();
